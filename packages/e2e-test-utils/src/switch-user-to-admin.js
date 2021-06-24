0aa99be5c6932a5d6a9c06f7b4dcf3c4fd725a5c
Bernie Reiter <ockham@raz.or.at>
2021-06-24 20:24:59 +0200

    switchUserToAdmin: Fix by actually evaluating current user

--- packages/e2e-test-utils/src/switch-user-to-admin.js
+++ packages/e2e-test-utils/src/switch-user-to-admin.js
@@ -2,14 +2,22 @@
  * Internal dependencies
  */
 import { loginUser } from './login-user';
-import { WP_USERNAME, WP_ADMIN_USER } from './shared/config';
+import { WP_ADMIN_USER } from './shared/config';
 
 /**
  * Switches the current user to the admin user (if the user
  * running the test is not already the admin user).
  */
 export async function switchUserToAdmin() {
-	if ( WP_USERNAME === WP_ADMIN_USER.username ) {
+	const currentDisplaynameElement = await page.$( '.display-name' );
+	if ( ! currentDisplaynameElement ) {
+		return;
+	}
+	const currentDisplayname = await page.evaluate(
+		( element ) => element.textContent,
+		currentDisplaynameElement
+	);
+	if ( currentDisplayname === WP_ADMIN_USER.username ) {
 		return;
 	}
 	await loginUser( WP_ADMIN_USER.username, WP_ADMIN_USER.password );
